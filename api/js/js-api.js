03d6b7b9667c2504ea0a3c336ebb1ecdca1ad363
Iván <ivanalex081195@gmail.com>
2023-05-15 23:56:32 -0600

    tabla api fetch

--- api/js/js-api.js
+++ api/js/js-api.js
@@ -5,8 +5,9 @@ fetch(url)
   .then((data) => mostrarData(data))
   .catch((error) => console.log(error));
  */
-
-data = [{"id":7,"email":"michael.lawson@reqres.in","first_name":"Michael","last_name":"Lawson","avatar":"https://reqres.in/img/faces/7-image.jpg"},{"id":8,"email":"lindsay.ferguson@reqres.in","first_name":"Lindsay","last_name":"Ferguson","avatar":"https://reqres.in/img/faces/8-image.jpg"},{"id":9,"email":"tobias.funke@reqres.in","first_name":"Tobias","last_name":"Funke","avatar":"https://reqres.in/img/faces/9-image.jpg"},{"id":10,"email":"byron.fields@reqres.in","first_name":"Byron","last_name":"Fields","avatar":"https://reqres.in/img/faces/10-image.jpg"},{"id":11,"email":"george.edwards@reqres.in","first_name":"George","last_name":"Edwards","avatar":"https://reqres.in/img/faces/11-image.jpg"},{"id":12,"email":"rachel.howell@reqres.in","first_name":"Rachel","last_name":"Howell","avatar":"https://reqres.in/img/faces/12-image.jpg"}];
+/* 
+data = [{"id":7,"email":"michael.lawson@reqres.in","first_name":"Michael","last_name":"Lawson","avatar":"https://reqres.in/img/faces/7-image.jpg"},{"id":8,"email":"lindsay.ferguson@reqres.in","first_name":"Lindsay","last_name":"Ferguson","avatar":"https://reqres.i
+n/img/faces/8-image.jpg"},{"id":9,"email":"tobias.funke@reqres.in","first_name":"Tobias","last_name":"Funke","avatar":"https://reqres.in/img/faces/9-image.jpg"},{"id":10,"email":"byron.fields@reqres.in","first_name":"Byron","last_name":"Fields","avatar":"https://reqres.in/img/faces/10-image.jpg"},{"id":11,"email":"george.edwards@reqres.in","first_name":"George","last_name":"Edwards","avatar":"https://reqres.in/img/faces/11-image.jpg"},{"id":12,"email":"rachel.howell@reqres.in","first_name":"Rachel","last_name":"Howell","avatar":"https://reqres.in/img/faces/12-image.jpg"}];
 console.log(data);
 console.log(data.length);
 const mostrarData = (data) => {
@@ -21,7 +22,9 @@ const mostrarData = (data) => {
         <td>${data[i].last_name}</td>
         <td>${data[i].avatar}</td></tr>`;
   }
-  document.getElementById("data").innerHTML = body;
+  document.getElementById('data').innerHTML = body;
+  const tableContainer = document.getElementById('data');
+    tableContainer.appendChild(table);
 };
 
 
@@ -39,6 +42,8 @@ const mostrarData = (data) => {
               
     });
     usersContainer.innerHTML = listUsers;
+    const tableContainer = document.getElementById('data');
+    tableContainer.appendChild(table);
 }
 
 const getUsers = async (url) =>{
@@ -51,3 +56,67 @@ const getUsers = async (url) =>{
         return err;
     }
 } 
+ */
+/* 
+// Coloca aquí el código JavaScript para obtener y tabular los datos
+fetch('https://reqres.in/api/users?page=2')
+  .then(response => response.json())
+  .then(data => {
+    const users = data.data; // Datos de los usuarios
+    const table = document.createElement('table'); // Crea la tabla
+
+    // Crea las filas y celdas de la tabla
+    users.forEach(user => {
+      const row = table.insertRow(); // Crea una fila
+
+      // Crea las celdas con los datos del usuario
+      const idCell = row.insertCell();
+      idCell.textContent = user.id;
+
+      const firstNameCell = row.insertCell();
+      firstNameCell.textContent = user.first_name;
+
+      const lastNameCell = row.insertCell();
+      lastNameCell.textContent = user.last_name;
+
+      const emailCell = row.insertCell();
+      emailCell.textContent = user.email;
+
+      const imageCell = row.insertCell();
+      imageCell.textContent = user.avatar;
+
+    });
+    const tableContainer = document.getElementById('data');
+    tableContainer.appendChild(table);
+  })
+  .catch(error => console.error(error)); */
+
+  const tableBody = document.querySelector("#user-table tbody");
+  fetch('https://reqres.in/api/users?delay=3')
+    .then(response => response.json())
+    .then(data => {
+      const users = data.data; // Datos de los usuarios
+
+      users.forEach(user => {
+        const row = tableBody.insertRow(); // Crea una fila
+
+        // Crea las celdas con los datos del usuario
+        const idCell = row.insertCell();
+        idCell.textContent = user.id;
+
+        const firstNameCell = row.insertCell();
+        firstNameCell.textContent = user.first_name;
+
+        const lastNameCell = row.insertCell();
+        lastNameCell.textContent = user.last_name;
+
+        const emailCell = row.insertCell();
+        emailCell.textContent = user.email;
+
+        const avatarCell = row.insertCell();
+        const avatarImage = document.createElement('img');
+        avatarImage.src = user.avatar;
+        avatarCell.appendChild(avatarImage);
+      });
+    })
+    .catch(error => console.error(error));
